f400b737c1ac0d850b88fa0d7e11bb8a9d2f8413
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2021-04-13 00:20:09 +0530

    screen creation2

---
+++ core/src/com/singh/spaceshooter/Spaceshooter.java
@@ -0,0 +1,41 @@
+package com.singh.spaceshooter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Spaceshooter extends Game {
+
+	GameScreen gameScreen;
+
+
+
+
+	@Override
+	public void create() {
+
+		gameScreen=new GameScreen();
+		setScreen(gameScreen);
+
+
+
+	}
+	@Override
+	public void dispose() {
+		super.dispose();
+		gameScreen.dispose();
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		gameScreen.resize(width, height);
+	}
+}
